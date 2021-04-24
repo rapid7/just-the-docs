c95dd3e0b9482b2522c30991248e0b54d7748dc9
Peter Mosses <18308236+pdmosses@users.noreply.github.com>
2021-04-24 08:19:26 +0200

    Fix search feature interaction with dark mode toggle
    - Change `showSearch` and `hideSearch` to use the `body` element instead of `document.documentElement`.
    
    Issue reported and fix provided by @odashevskii-plaid, see https://github.com/pmarsceill/just-the-docs/pull/578#issuecomment-826018190

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -74,7 +74,7 @@ function initSearch() {
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
       var docs = JSON.parse(request.responseText);
-      
+
       lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
 
       var index = lunr(function(){
@@ -120,12 +120,14 @@ function searchLoaded(index, docs) {
   var currentInput;
   var currentSearchIndex = 0;
 
+  var body = document.querySelector('body');
+
   function showSearch() {
-    document.documentElement.classList.add('search-active');
+    body.classList.add('search-active');
   }
 
   function hideSearch() {
-    document.documentElement.classList.remove('search-active');
+    body.classList.remove('search-active');
   }
 
   function update() {
